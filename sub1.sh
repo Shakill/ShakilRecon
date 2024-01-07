4f9c02e1e706317f7e10056f2b6498297bcead4f
MD SHAHADAT HOSSAIN <m.shakil.bd11@gmail.com>
2024-01-08 00:15:40 +0600

    Update sub1.sh

--- sub1.sh
+++ sub1.sh
@@ -92,7 +92,7 @@ sub_brutforce(){
 for domain in $(cat $host);
 do
 puredns bruteforce /root/wordlist/$domain_my_wordlist.txt -d /root/recon/$domain/subdomain/good/final/best/sub_brutforce_file.txt -r /root/wordlist/resolvers.txt | tee -a /root/recon/$domain/subdomain/good/final/best/puredns_sub_by_my_wordlist.txt
-cat /root/recon/$domain/subdomain/good/final/best/sub_brutforce_file.txt | dnsgen - | puredns resolve --resolvers /root/wordlist/resolvers.txt | tee -a /root/recon/$domain/subdomain/good/final/best/dnsgen_puredns_sub.txt
+#cat /root/recon/$domain/subdomain/good/final/best/sub_brutforce_file.txt | dnsgen - | puredns resolve --resolvers /root/wordlist/resolvers.txt | tee -a /root/recon/$domain/subdomain/good/final/best/dnsgen_puredns_sub.txt
 done
 }
 sub_brutforce
